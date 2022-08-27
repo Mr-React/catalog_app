0d5fca6193b0b5e5dabdc097ce479e77dff7e8ae
Mr-React <ambureomkar@gmail.com>
2022-08-27 18:56:27 +0530

    Created new custom widget for homepage

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("Welcome to flutter learn"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
